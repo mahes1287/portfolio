08613abe91e992897f7dede30d44e45fa7a91df3
maheshwaranv <mahes1287@gmail.com>
2023-01-10 23:36:11 +0100

    hero added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { Inter } from "@next/font/google";
 import Header from "../components/Header";
+import Hero from "../components/Hero";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,10 +15,9 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        {/* Header */}
         <Header />
         {/* Hero */}
-
+        <Hero />
         {/* About */}
 
         {/* Experience */}
