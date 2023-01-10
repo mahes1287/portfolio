dc3f239a8407617f5d2fc59cf583f794ced01346
maheshwaranv <mahes1287@gmail.com>
2023-01-10 23:01:53 +0100

    icons added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,12 +1,36 @@
 import React from "react";
+import { SocialIcon } from "react-social-icons";
 
 type Props = {};
 
 export default function Header({}: Props) {
   return (
     <header>
-          <div className="flex flex-row items-center">
-              
+      <div className="flex flex-row items-center">
+        <SocialIcon
+          url="https://twitter.com/mahes1287"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://linkedin.com/in/maheshwaranv"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <SocialIcon
+          url="https://github.com/mahes1287"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+      </div>
+      <div>
+        <SocialIcon
+          className="cursor-pointer"
+          network="email"
+          fgColor="gray"
+          bgColor="transparent"
+        />
+        <p className="uppercase hidden md:inline-flex text-sm">Get in touch</p>
       </div>
     </header>
   );
