25249eb7619176481c95afd6a3c6f68910d7b2cc
maheshwaranv <mahes1287@gmail.com>
2023-01-10 23:17:08 +0100

    pre framer motion

--- components/Header.tsx
+++ components/Header.tsx
@@ -5,7 +5,7 @@ type Props = {};
 
 export default function Header({}: Props) {
   return (
-    <header>
+    <header className="flex p-5 items-start justify-between mx-auto sticky top-0 max-w-7xl z-20 xl:items-center ">
       <div className="flex flex-row items-center">
         <SocialIcon
           url="https://twitter.com/mahes1287"
@@ -23,14 +23,16 @@ export default function Header({}: Props) {
           bgColor="transparent"
         />
       </div>
-      <div>
+      <div className="cursor-pointer flex flex-row items-center">
         <SocialIcon
           className="cursor-pointer"
           network="email"
           fgColor="gray"
           bgColor="transparent"
         />
-        <p className="uppercase hidden md:inline-flex text-sm">Get in touch</p>
+        <p className="uppercase hidden md:inline-flex text-sm text-gray-500">
+          Get in touch
+        </p>
       </div>
     </header>
   );
