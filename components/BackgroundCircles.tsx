33603d4471ab1a839d6a17d106e0415e27100fe4
maheshwaranv <mahes1287@gmail.com>
2023-01-11 08:41:11 +0100

    for hero component

---
+++ components/BackgroundCircles.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+type Props = {};
+
+export default function BackgroundCircles({}: Props) {
+  return (
+    <div>
+      <div className="border border-[#333333] animate-ping w-[200px] h-[200px] rounded-full mt-5" />
+    </div>
+  );
+}
