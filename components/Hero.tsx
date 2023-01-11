3579f6cd54c0c2acd1613c07f819f2011408114f
maheshwaranv <mahes1287@gmail.com>
2023-01-11 08:40:48 +0100

    hero updated

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,7 +1,24 @@
 import React from "react";
+import { Cursor, useTypewriter } from "react-simple-typewriter";
+import BackgroundCorcles from "./BackgroundCircles";
 
 type Props = {};
 
 export default function Hero({}: Props) {
-  return <div>Hero</div>;
+  const [text, count] = useTypewriter({
+    words: ["Hi, am Mahesh", "<More-Coffee-More-Code.tsx />"],
+    loop: true,
+    delaySpeed: 2000,
+  });
+  return (
+    <div>
+      <BackgroundCorcles />
+      <h1>
+        <p>
+          {text}
+          <Cursor cursorColor="#F75" />
+        </p>
+      </h1>
+    </div>
+  );
 }
